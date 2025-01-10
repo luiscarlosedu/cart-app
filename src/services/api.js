1f441fb0cf94a6e24abb3d9d505de7bc2843cd20
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-01-10 14:48:21 -0300

    fix: variáveis ambiente corretas

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
     // baseURL: 'http://localhost:3000/'
-    baseURL: process.env.IP_CONFIG
+    baseURL: process.env.EXPO_PUBLIC_IP_CONFIG
 });
